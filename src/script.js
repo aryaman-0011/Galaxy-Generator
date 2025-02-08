2f9fb2bb1e98659e488513ad10bd0e86b947c98c
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-02-08 21:29:55 +0530

    Adding spin to galaxy

--- src/script.js
+++ src/script.js
@@ -23,6 +23,7 @@ parameters.count = 100000
 parameters.size = 0.01
 parameters.radius = 5
 parameters.branches = 3
+parameters.spin = 1
 
 let geometry = null
 let material = null
@@ -50,13 +51,14 @@ const generateGalaxy = () => {
         const i3 = i * 3
 
         const radius = Math.random() * parameters.radius
+        const spinAngle = radius * parameters.spin
         const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
 
 
 
-        positions[i3 + 0] = Math.cos(branchAngle) * radius
+        positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius
         positions[i3 + 1] = 0
-        positions[i3 + 2] = Math.sin(branchAngle) * radius
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius
     }
 
     geometry.setAttribute(
@@ -87,6 +89,7 @@ gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(gene
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin').min(- 5).max(5).step(0.001).onFinishChange(generateGalaxy)
 
 /**
  * Sizes
